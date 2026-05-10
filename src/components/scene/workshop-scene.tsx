16eacc623267bf350c8a31b1bfb6ddd6b8c4cdc6
HasNate618 <nate.e.espejo@gmail.com>
2026-05-10 09:11:27 -0400

    fix: scale FBX model 0.5x, address double render

--- src/components/scene/workshop-scene.tsx
+++ src/components/scene/workshop-scene.tsx
@@ -215,7 +215,7 @@ function AgentCharacter({
     <group ref={groupRef} position={[position[0], 0, position[2]]}>
       <FBXModelLoader
         url="/3d/goobs.fbx"
-        scale={[0.01, 0.01, 0.01]}
+        scale={[0.005, 0.005, 0.005]}
         position={[0, 0, 0]}
         animationState={state}
         color={color}
